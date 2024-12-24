756dbb1443ab6e1a3f65768e7f3f462f960c46b6
RoyPery2010 <roypery2010@gmail.com>
2024-12-24 17:20:45 +0200

    Added gitignore and README, and modified the JMP Instructions

--- rvm.c
+++ rvm.c
@@ -19,8 +19,9 @@ typedef enum {
     INST_CMPL,
     INST_CMPGE,
     INST_CMPLE,
-    INST_CJMP,
     INST_JMP,
+    INST_ZJMP,
+    INST_NZJMP,
     INST_PRINT,
     INST_HALT,
 } Inst_Set;
@@ -53,17 +54,17 @@ typedef struct {
 #define DEF_INST_CMPGE() {.type = INST_CMPGE}
 #define DEF_INST_CMPLE() {.type = INST_CMPLE}
 #define DEF_INST_JMP(x) {.type = INST_JMP, .value = x}
-#define DEF_INST_CJMP(x) {.type = INST_CJMP, .value = x}
+#define DEF_INST_ZJMP(x) {.type = INST_ZJMP, .value = x}
+#define DEF_INST_NZJMP(x) {.type = INST_NZJMP, .value = x}
 #define DEF_INST_PRINT() {.type = INST_PRINT}
 #define DEF_INST_HALT() {.type = INST_HALT}
 
 Inst program[] = {
     DEF_INST_PUSH(14),
-    DEF_INST_PUSH(27),
+    DEF_INST_PUSH(5),
+    DEF_INST_NZJMP(6),
     DEF_INST_HALT(),
-    DEF_INST_JMP(2),
     DEF_INST_PUSH(15),
-    DEF_INST_CMPGE(),
     DEF_INST_NOP(),
     DEF_INST_NOP(),
     DEF_INST_NOP(),
@@ -228,26 +229,37 @@ int main() {
                     push(loaded_machine, 0);
                 }
                 break;
-	    case INST_CMPGE:                                                        a = pop(loaded_machine);                                           b = pop(loaded_machine);                                           push(loaded_machine, b);                                           push(loaded_machine, a);
-		if (a >= b) {
-		   push(loaded_machine, 1);
-		} else {
-		   push(loaded_machine, 0);
-		}
-		break;
-	    case INST_CMPLE:
-		a = pop(loaded_machine);
-		b = pop(loaded_machine);
-		push(loaded_machine, b);
-		push(loaded_machine, a);
-		if (a <= b) {
-		  push(loaded_machine, 1);
-		} else {
-		  push(loaded_machine, 0);
-		}
-		break;
-            case INST_CJMP:
-                if (pop(loaded_machine) == 1) {
+	        case INST_CMPGE:
+                a = pop(loaded_machine);                                           
+                b = pop(loaded_machine);                                           
+                push(loaded_machine, b);                                           
+                push(loaded_machine, a);
+		        if (a >= b) {
+		            push(loaded_machine, 1);
+		        } else {
+		        push(loaded_machine, 0);
+		        }
+		        break;
+	        case INST_CMPLE:
+		        a = pop(loaded_machine);
+		        b = pop(loaded_machine);
+		        push(loaded_machine, b);
+		        push(loaded_machine, a);
+		        if (a <= b) {
+		            push(loaded_machine, 1);
+		        } else {
+		            push(loaded_machine, 0);
+		        }
+		        break;
+            case INST_JMP:
+                ip = loaded_machine->instructions[ip].value - 1;
+                if (ip + 1 >= loaded_machine->program_size) {
+                        fprintf(stderr, "ERROR: Cannot jump out of bounds\n");
+                        exit(1);
+                    }
+                break;
+            case INST_ZJMP:
+                if (pop(loaded_machine) == 0) {
                     printf("IP: %d\n", ip);
                     ip = loaded_machine->instructions[ip].value - 1;
                     if (ip + 1 >= loaded_machine->program_size) {
@@ -259,12 +271,18 @@ int main() {
                     continue;
                 }
                 break;
-            case INST_JMP:
-                ip = loaded_machine->instructions[ip].value - 1;
-                if (ip + 1 >= loaded_machine->program_size) {
+            case INST_NZJMP:
+                if (pop(loaded_machine) != 0) {
+                    printf("IP: %d\n", ip);
+                    ip = loaded_machine->instructions[ip].value - 1;
+                    if (ip + 1 >= loaded_machine->program_size) {
                         fprintf(stderr, "ERROR: Cannot jump out of bounds\n");
                         exit(1);
                     }
+                    printf("IP: %d\n", ip);
+                } else {
+                    continue;
+                }
                 break;
             case INST_PRINT:
                 printf("%d\n", pop(loaded_machine));
