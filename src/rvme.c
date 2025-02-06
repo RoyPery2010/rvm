16a02e199c6fd1e7370b2ab18d2f827ed76137c8
RoyPery2010 <roypery2010@gmail.com>
2025-02-06 18:31:18 +0200

    Introduce rvm_execute_program() and clean up CLI parsing for rvme

--- src/rvme.c
+++ src/rvme.c
@@ -1,14 +1,54 @@
 #include "./rvm.h"
+char *shift(int *argc, char ***argv) {
+    assert(*argc > 0);
+    char *result = **argv;
+    *argv += 1;
+    *argc -= 1;
+    return result;
+}
+void usage(FILE *stream, const char *program) {
+    fprintf(stream, "Usage: %s -i <input.rvm> [-l <limit>]\n", program);
+}
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: ./rvme <input.rvm>\n");
-        fprintf(stderr, "ERROR: expected input\n");
+    const char *program = shift(&argc, &argv);
+    const char *input_file_path = NULL;
+    int limit = -1;
+
+    while (argc > 0) {
+        const char *flag = shift(&argc, &argv);
+        if (strcmp(flag, "-i") == 0) {
+            if (argc == 0) {
+                usage(stderr, program);
+                fprintf(stderr, "ERROR: No argument is provided for flag `%s`\n", flag);
+                exit(1);
+            }
+            input_file_path = shift(&argc, &argv);
+        } else if (strcmp(flag, "-l") == 0) {
+            if (argc == 0) {
+                usage(stderr, program);
+                fprintf(stderr, "ERROR: No argument is provided for flag `%s`\n", flag);
+                exit(1);
+            }
+            limit = atoi(shift(&argc, &argv));
+        } else if (strcmp(flag, "-h") == 0) {
+            usage(stderr, program);
+            exit(0);
+        } else {
+            usage(stderr, program);
+            fprintf(stderr, "ERROR: Unknown flag `%s`\n", flag);
+            exit(1);
+        }
+    }
+
+    if (input_file_path == NULL) {
+        usage(stderr, program);
+        fprintf(stderr, "ERROR: input was not provided\n");
         exit(1);
     }
     RVM rvm = {0};
-    rvm_load_program_from_file(&rvm, argv[1]);
-    Err err = rvm_execute_program(&rvm, 69);
+    rvm_load_program_from_file(&rvm, input_file_path);
+    Err err = rvm_execute_program(&rvm, limit);
     rvm_dump_stack(stdout, &rvm);
     if (err != ERR_OK) {
         fprintf(stderr, "ERROR: %s\n", err_as_cstr(err));
